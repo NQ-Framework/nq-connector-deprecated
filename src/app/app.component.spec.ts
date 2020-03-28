a7cf40d7f3f2b806e5c431a0fce605ce475a632b
Milos Spasojevic <milos.spasojevic.tms@gmail.com>
2020-03-28 15:21:57 +0100

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ElectronService } from 'ngx-electron';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [
+        { provide: ElectronService, useValue: { isElectronApp: false } }
+      ]
     }).compileComponents();
   }));
 
@@ -30,6 +30,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('nq-connector app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'nq-connector app is running!'
+    );
   });
 });
