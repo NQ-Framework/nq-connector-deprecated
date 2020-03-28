1f60118d31ef632f9b0e314332f3c958e9e70362
Milos Spasojevic <milos.spasojevic.tms@gmail.com>
2020-03-28 18:58:43 +0100

    Switched to electron-forge

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,38 +1,30 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 
-let mainWindow: Electron.BrowserWindow;
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  app.quit();
+}
 
 app.allowRendererProcessReuse = true;
 
-function createWindow() {
+const createWindow = () => {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     height: 600,
+    width: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
-    },
-    width: 800
+    }
   });
 
   // and load the index.html of the app.
-  // mainWindow.loadURL('http://localhost:4200/');
-   mainWindow.loadFile(
-     path.join(__dirname, '../nq-connector/index.html')
-   );
+  mainWindow.loadFile(path.join(__dirname, '../nq-connector/index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-}
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -49,12 +41,12 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it"s common to re-create a window in the app when the
+  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
