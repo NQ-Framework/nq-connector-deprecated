fd23e50eaa5aecf543d893e1caafb53a2acff0ca
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-04-20 16:49:59 +0200

    fix test

--- src/app/organization/create-organization/create-organization.component.spec.ts
+++ src/app/organization/create-organization/create-organization.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CreateOrganizationComponent } from './create-organization.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 describe('Create Organization Component', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, FormsModule],
+      providers: [
+        { provide: AngularFirestore, useValue: {} },
+        { provide: AngularFireAuth, useValue: {} },
+      ],
       declarations: [CreateOrganizationComponent],
     }).compileComponents();
   }));
