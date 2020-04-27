29b3a19ab63b86dc104ac5fef12829b78625790b
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-04-27 22:36:20 +0200

    WIP organization component

--- src/app/organization/organization.component.spec.ts
+++ src/app/organization/organization.component.spec.ts
@@ -2,7 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrganizationComponent } from './organization.component';
 // import { RouterTestingModule } from '@angular/router/testing';
-// import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Subject, of } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { first } from 'rxjs/operators';
+import { Organization } from '@nqframework/models';
 
 describe('OrganizationComponent', () => {
   let component: OrganizationComponent;
@@ -11,18 +15,82 @@ describe('OrganizationComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       // imports: [RouterTestingModule],
-      // providers: [{ provide: AngularFireAuth, useValue: {} }],
+      providers: [
+        {
+          provide: AngularFirestore,
+          useValue: jasmine.createSpyObj(['collection']),
+        },
+        {
+          provide: AngularFireAuth,
+          useValue: { authState: new Subject() },
+        },
+      ],
       declarations: [OrganizationComponent],
     }).compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(() => {});
+
+  it('should create', () => {
     fixture = TestBed.createComponent(OrganizationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
 
-  it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should get all organizations for user', async(async () => {
+    const mockOrganizations: Organization[] = [
+      {
+        name: 'Organization name',
+        email: 'organization@email.com',
+        members: [
+          {
+            uid: 'user id',
+            displayName: 'user',
+            email: 'user@user.com',
+            status: 'accepted',
+            role: 'admin',
+          },
+        ],
+        memberIds: ['user id'],
+        address1: '',
+        address2: '',
+        country: '',
+      },
+    ];
+    const auth: any = TestBed.inject(AngularFireAuth);
+
+    const firestore: any = TestBed.inject(AngularFirestore);
+    const valueChangesSpy = jasmine.createSpyObj(['valueChanges']);
+    valueChangesSpy.valueChanges.and.returnValue(of(mockOrganizations));
+    firestore.collection.and.callFake((path, ref) => {
+      expect(path).toEqual('organizations');
+      const refSpy = jasmine.createSpyObj(['where']);
+      ref(refSpy);
+      expect(refSpy.where).toHaveBeenCalledWith(
+        'memberIds',
+        'array-contains',
+        'user id',
+      );
+      return valueChangesSpy;
+    });
+    fixture = TestBed.createComponent(OrganizationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    component.organizations$.pipe(first()).subscribe((orgs) => {
+      expect(orgs).toEqual(mockOrganizations);
+    });
+    await fixture.whenStable();
+    auth.authState.next({ uid: 'user id' });
+    expect(firestore.collection).toHaveBeenCalledWith(
+      'organizations',
+      jasmine.any(Function),
+    );
+    firestore.collection.calls.reset();
+    component.organizations$.pipe(first()).subscribe((orgs) => {
+      expect(orgs).toEqual([]);
+    });
+    auth.authState.next(null);
+  }));
 });
--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { switchMap, share } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
+import { Organization } from '@nqframework/models';
 
 @Component({
   selector: 'app-organization',
@@ -6,6 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organization.component.scss'],
 })
 export class OrganizationComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+  organizations$: Observable<Organization[]>;
+  constructor(
+    private firestore: AngularFirestore,
+    private auth: AngularFireAuth,
+  ) {}
+  ngOnInit() {
+    this.organizations$ = this.auth.authState.pipe(
+      share(),
+      switchMap((user) => {
+        if (!user) {
+          return of([]);
+        }
+        console.log('uid je', user.uid);
+        return this.firestore
+          .collection<Organization>('organizations', (ref) =>
+            ref.where('memberIds', 'array-contains', user.uid),
+          )
+          .valueChanges();
+      }),
+    );
+
+    this.organizations$.subscribe((val) => {
+      console.log(val);
+    });
+  }
 }
